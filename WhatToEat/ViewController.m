4b7a334bab44533bfb5af4963b4f8770dc57663e
YinShi <arron_ys@hotmail.com>
2016-10-24 14:09:27 +0800

    Initial Commit

---
+++ WhatToEat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WhatToEat
+//
+//  Created by YinShi on 16/10/24.
+//  Copyright © 2016年 YS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
